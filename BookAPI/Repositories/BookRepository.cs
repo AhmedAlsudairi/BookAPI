79cd08c08486014bc3904708c1b4a41288cfc8b3
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-07-30 18:01:47 +0300

    creating BookRepository class

---
+++ BookAPI/Repositories/BookRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using BookAPI.Models;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _contesxt;
+        public BookRepository(BookContext context)
+        {
+            _contesxt = context;
+        }
+
+        Task<Book> IBookRepository.Create(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IBookRepository.Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEquatable<Book>> IBookRepository.Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Book> IBookRepository.Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IBookRepository.Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BookAPI/Repositories/IBookRepository.cs
+++ BookAPI/Repositories/IBookRepository.cs
@@ -1,7 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+using BookAPI.Models;
 namespace BookAPI.Repositories
 {
     public interface IBookRepository
     {
+        Task<IEquatable<Book>> Get();
+
+        Task<Book> Get(int Id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int Id);
     }
 }
