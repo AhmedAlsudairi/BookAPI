c4202aaac38a7410455af7785917fe6005e470d9
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-07-30 17:58:10 +0300

    creating IBookRepository interface

---
+++ BookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+    }
+}
--- BookAPI/Startup.cs
+++ BookAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,7 @@ namespace BookAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<BookContext>(o => o.UseSqlite("Data source=books.db"));
             services.AddControllers();
         }
 
